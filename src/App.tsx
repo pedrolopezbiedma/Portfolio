8f3006baa66914298752fc088a6d3f098aba4bd6
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-03-31 15:45:48 +0200

    Include first version of Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
-import Button from './components/Button';
+import Header from './components/Header';
 
 const App = () => {
 	return (
 		<>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-			<ButtonGradient />
-
 			<div className='pt-[4.75rem] lg:pt-[5.25] overflow-hidden'>
-				<Button
-					className='mt-10'
-					href='#login'>
-					Some link
-				</Button>
-				<Button className='mt-10'>Some button</Button>
+				<Header />
 			</div>
+			<ButtonGradient />
 		</>
 	);
 };
