00db9d7d6da7309d10354072f3cecd2430c062f7
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-03-31 22:38:36 +0200

    Migrate provided components to typescript

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,16 @@ import ButtonSvg from '../assets/svg/ButtonSvg';
 interface ButtonProps {
 	className: string;
 	href?: string;
-	children?: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	children?: any;
 	px?: string;
 	py?: string;
 	white?: boolean;
 	onClick?: () => void;
 }
 
-const Button = ({ className, href, children, px, white }: ButtonProps) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Button = ({ className, href, children, px, white, onClick }: ButtonProps) => {
 	const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1
 		${px || 'px-7'}
 		${white ? 'text-n-8' : 'text-n-1'}
@@ -29,7 +31,9 @@ const Button = ({ className, href, children, px, white }: ButtonProps) => {
 				</a>
 			)}
 			{!href && (
-				<button className={classes}>
+				<button
+					onClick={onClick}
+					className={classes}>
 					<span className={spanClasses}> {children}</span>
 					{ButtonSvg(white)}
 				</button>
