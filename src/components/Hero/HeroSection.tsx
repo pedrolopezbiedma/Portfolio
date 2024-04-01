09eb9b909c68affdb65133eb3c33975afbb27ed7
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-04-01 10:23:14 +0200

    More Styling into the Hero Section

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -1,12 +1,12 @@
 import Section from '../shared/Section';
-import { curve } from '../../assets';
+import { curve, heroBackground, robot } from '../../assets';
 import Button from '../shared/Button';
 
 const HeroSection = () => {
 	return (
 		<Section
 			id='hero'
-			className='pt-[12rem] -mt-[5.25]'
+			className='pt-[12rem] -mt-[5.25rem]'
 			crosses={false}
 			crossesOffset='lg:translate-y-[5.25rem]'
 			customPaddings>
@@ -35,7 +35,31 @@ const HeroSection = () => {
 						Get Started
 					</Button>
 				</div>
-				<div className='relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24'></div>
+				<div className='relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24'>
+					<div className='relative z-1 p-0.5 roundeed-2xl bg-conic-gradient'>
+						<div className='relative bg-n-8 rounded-[1rem]'>
+							<div className='h-[1.4rem] bg-n-10 rounded-t-[0.9rem]' />
+							<div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
+								<img
+									className='w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]'
+									width={1024}
+									height={490}
+									alt='AI'
+									src={robot}
+								/>
+							</div>
+						</div>
+					</div>
+					<div className='absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%] '>
+						<img
+							className='w-full'
+							src={heroBackground}
+							width={1440}
+							height={1800}
+							alt='Hero'
+						/>
+					</div>
+				</div>
 			</div>
 		</Section>
 	);
