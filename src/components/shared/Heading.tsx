c48a9fd62a570625e898cdf41c12904128c58673
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-04-02 11:35:44 +0200

    Update Heading component to accept tags

--- src/components/shared/Heading.tsx
+++ src/components/shared/Heading.tsx
@@ -1,12 +1,16 @@
+import Tagline from './Tagline';
+
 interface HeadingProps {
 	className?: string;
 	title: string;
 	text?: string;
+	tag?: string;
 }
 
-const Heading = ({ className, title, text }: HeadingProps) => {
+const Heading = ({ className, title, text, tag }: HeadingProps) => {
 	return (
 		<div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 text-center`}>
+			{tag && <Tagline className='mb-4 md:justify-center'>{tag}</Tagline>}
 			{title && <h2 className='h2'>{title}</h2>}
 			{text && <p className='body-2 mt-4 text-n-4'>{text}</p>}
 		</div>
