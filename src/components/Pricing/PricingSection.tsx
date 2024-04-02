f4ca5ab2f917a92d19bb1fd10694eb09d12f5195
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-04-02 12:04:33 +0200

    Finish the Pricing section

--- src/components/Pricing/PricingSection.tsx
+++ src/components/Pricing/PricingSection.tsx
@@ -42,6 +42,14 @@ const PricingSection = () => {
 					<PricingList />
 					<RightLine />
 				</div>
+
+				<div className='flex justify-center mt-10'>
+					<a
+						className='text-xs font-code font-bold tracking-wider uppercase border-b'
+						href='/pricing'>
+						See the full details
+					</a>
+				</div>
 			</div>
 		</Section>
 	);
